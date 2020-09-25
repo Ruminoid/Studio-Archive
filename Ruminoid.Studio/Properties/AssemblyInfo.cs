999f0d007a8e2f9cd832e9a5eeb70df0836eb534
Il Harper <afanyiyu@hotmail.com>
2020-09-25 08:42:21 +0800

    feat: Assembly Info Update

--- Ruminoid.Studio/Properties/AssemblyInfo.cs
+++ Ruminoid.Studio/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Ruminoid.Studio")]
+[assembly: AssemblyTitle("Ruminoid Studio Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ruminoid.Studio")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCompany("Ruminoid")]
+[assembly: AssemblyProduct("Ruminoid Studio Core")]
+[assembly: AssemblyCopyright("Copyright © Ruminoid 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.3.1.0")]
+[assembly: AssemblyFileVersion("0.3.1.0")]
