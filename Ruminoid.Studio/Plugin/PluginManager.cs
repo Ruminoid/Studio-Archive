13aa882cbb0ba3d21039419961536f3d5cf95e06
Il Harper <afanyiyu@hotmail.com>
2020-09-25 08:49:12 +0800

    feat: Add PluginManager Class

---
+++ Ruminoid.Studio/Plugin/PluginManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ruminoid.Studio.Plugin
+{
+    public sealed class PluginManager
+    {
+        #region Current
+
+        public static PluginManager Current { get; } = new PluginManager();
+
+        #endregion
+
+        #region Constructor
+
+        private PluginManager()
+        {
+
+        }
+
+        #endregion
+    }
+}
