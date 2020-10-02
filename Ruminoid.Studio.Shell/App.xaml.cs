7d98971367d6cc0c5f792b8e461e4852154e7272
Il Harper <afanyiyu@hotmail.com>
2020-10-02 15:00:43 +0800

    feat: Add OnStartup Method

--- Ruminoid.Studio.Shell/App.xaml.cs
+++ Ruminoid.Studio.Shell/App.xaml.cs
@@ -13,5 +13,9 @@ namespace Ruminoid.Studio.Shell
     /// </summary>
     public partial class App : Application
     {
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            
+        }
     }
 }
