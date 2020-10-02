bc079ae035bc88d302392fd4bfd1bfbc62f3d7a8
Il Harper <afanyiyu@hotmail.com>
2020-10-02 10:13:12 +0800

    feat: IInterfacePlugin Update

--- Ruminoid.Studio/Plugin/IInterfacePlugin.cs
+++ Ruminoid.Studio/Plugin/IInterfacePlugin.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using YDock.Interface;
 
 namespace Ruminoid.Studio.Plugin
 {
     public interface IInterfacePlugin : INamed
     {
+        Collection<IDockSource> Panels { get; }
+
+        Collection<RoutedUICommand> Commands { get; }
     }
 }
