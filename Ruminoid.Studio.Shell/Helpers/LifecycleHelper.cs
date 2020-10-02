ab890367ed93f014b8d711407b2cad6809cd0df9
Il Harper <afanyiyu@hotmail.com>
2020-10-02 15:16:12 +0800

    feat: Add HandleParseError Method

--- Ruminoid.Studio.Shell/Helpers/LifecycleHelper.cs
+++ Ruminoid.Studio.Shell/Helpers/LifecycleHelper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommandLine;
+using Ookii.Dialogs.Wpf;
 
 namespace Ruminoid.Studio.Shell.Helpers
 {
@@ -23,7 +24,29 @@ namespace Ruminoid.Studio.Shell.Helpers
 
         private static void HandleParseError(IEnumerable<Error> obj)
         {
-            
+            StringBuilder stringBuilder = new StringBuilder("错误详细信息：");
+
+            foreach (Error error in obj)
+            {
+                stringBuilder.Append(error.Tag);
+                stringBuilder.Append(" | ");
+            }
+
+            new TaskDialog
+            {
+                EnableHyperlinks = false,
+                MainInstruction = "未能解析的命令行参数。请考虑检查调用方。",
+                WindowTitle = "错误",
+                Content = stringBuilder.ToString(),
+                MainIcon = TaskDialogIcon.Error,
+                MinimizeBox = false,
+                Buttons =
+                {
+                    new TaskDialogButton(ButtonType.Ok)
+                }
+            }.ShowDialog();
+
+            Environment.Exit(1);
         }
 
         public static void CleanUp()
