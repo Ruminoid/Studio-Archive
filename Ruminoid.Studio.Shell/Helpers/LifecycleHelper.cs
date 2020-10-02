614397292f64538a26c00fafef08622f4fd5d4a5
Il Harper <afanyiyu@hotmail.com>
2020-10-02 15:07:34 +0800

    feat: Add CommandLineParser in StartUp

--- Ruminoid.Studio.Shell/Helpers/LifecycleHelper.cs
+++ Ruminoid.Studio.Shell/Helpers/LifecycleHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommandLine;
 
 namespace Ruminoid.Studio.Shell.Helpers
 {
@@ -10,12 +11,29 @@ namespace Ruminoid.Studio.Shell.Helpers
     {
         public static void StartUp(string[] args)
         {
+            Parser.Default.ParseArguments<CommandLineOptions>(args)
+                .WithParsed(RunOptions)
+                .WithNotParsed(HandleParseError);
+        }
 
+        private static void RunOptions(CommandLineOptions obj)
+        {
+            
+        }
+
+        private static void HandleParseError(IEnumerable<Error> obj)
+        {
+            
         }
 
         public static void CleanUp()
         {
 
         }
+    }
+
+    public sealed class CommandLineOptions
+    {
+
     }
 }
