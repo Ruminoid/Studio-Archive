ed98f37948688450fe420d7e540efa03bb70caed
Il Harper <afanyiyu@hotmail.com>
2020-10-02 10:03:28 +0800

    feat: Add ITransform Interface

---
+++ Ruminoid.Studio/Plugin/ITransform.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ruminoid.Studio.Project;
+
+namespace Ruminoid.Studio.Plugin
+{
+    public interface ITransform : INamed
+    {
+        string Convert(RumiItem item);
+    }
+}
