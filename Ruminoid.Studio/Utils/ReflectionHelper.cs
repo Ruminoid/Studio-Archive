060a463b0b99290623937a26b523dbba659b8339
Il Harper <afanyiyu@hotmail.com>
2020-10-03 14:35:50 +0800

    feat: Add ReflectionHelper Class

---
+++ Ruminoid.Studio/Utils/ReflectionHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace Ruminoid.Studio.Utils
+{
+    public static class ReflectionHelper
+    {
+        public static string GetAssemblyName(object context) =>
+            context.GetType().Assembly.GetName().Name;
+
+        public static string GetFullName(object context) =>
+            $"{GetAssemblyName(context)}.{context.GetType()}";
+    }
+}
