add500141b46cc68c7000952c755f5c31821a545
Il Harper <afanyiyu@hotmail.com>
2020-10-03 14:29:07 +0800

    feat: Add LogHelper Class

---
+++ Ruminoid.Studio/Utils/LogHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.Composition;
+
+namespace Ruminoid.Studio.Utils
+{
+    [Export]
+    public sealed class LogHelper : IDisposable
+    {
+        public void Dispose()
+        {
+        }
+    }
+}
