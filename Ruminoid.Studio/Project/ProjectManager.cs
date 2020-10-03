05109358e58800ba533957d9520c8229ae5a1638
Il Harper <afanyiyu@hotmail.com>
2020-10-03 15:37:31 +0800

    feat: Add ProjectManager Class

---
+++ Ruminoid.Studio/Project/ProjectManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ruminoid.Studio.Project
+{
+    public sealed class ProjectManager
+    {
+        public RumiProject Project { get; private set; }
+
+        public void Load(string projectPath)
+        {
+
+        }
+    }
+}
