1e5c0faa62fb0ddc2904b9eae60e066d42216733
Il Harper <afanyiyu@hotmail.com>
2020-10-08 16:50:06 +0800

    feat: Add ActionManager

---
+++ Ruminoid.Studio/Transaction/ActionManager.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.Composition;
+using MvvmCross.ViewModels;
+using Ruminoid.Studio.Project;
+
+namespace Ruminoid.Studio.Transaction
+{
+    [Export]
+    public sealed class ActionManager : MvxNotifyPropertyChanged
+    {
+        #region Constructor
+
+        [ImportingConstructor]
+        public ActionManager(ProjectManager projectManager)
+        {
+            _projectManager = projectManager;
+        }
+
+        #endregion
+
+        private ProjectManager _projectManager;
+    }
+}
