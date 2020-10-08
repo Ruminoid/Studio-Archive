8f7fbdf2fb76cbfc10d6ba712abbf6c9b4743a07
Il Harper <afanyiyu@hotmail.com>
2020-10-08 17:26:13 +0800

    feat: Add DelegateAction<T> Class

--- Ruminoid.Studio/Transaction/DelegateAction.cs
+++ Ruminoid.Studio/Transaction/DelegateAction.cs
@@ -58,4 +58,61 @@ namespace Ruminoid.Studio.Transaction
 
         #endregion
     }
+
+    public class DelegateAction<T> : ActionBase
+    {
+        #region Constructor
+
+        public DelegateAction(
+            Action<T> execute,
+            Action<T> unExecute,
+            T state,
+            string name = "",
+            string icon = "")
+        {
+            _execute = execute;
+            _unExecute = unExecute;
+            _state = state;
+            if (!string.IsNullOrEmpty(name)) Name = name;
+            if (!string.IsNullOrEmpty(icon)) Icon = icon;
+        }
+
+        #endregion
+
+        #region Data
+
+        private Action<T> _execute;
+
+        private Action<T> _unExecute;
+
+        private T _state;
+
+        #endregion
+
+        #region Override
+
+        public new string Name { get; } = "（未知操作）";
+
+        public new string Icon { get; } = "DocumentUnknown";
+
+        #endregion
+
+        #region Operations
+
+        public override void Execute()
+        {
+            if (_execute is null)
+                throw new ArgumentNullException(nameof(_execute));
+            _execute(_state);
+        }
+
+        public override void UnExecute()
+        {
+            if (_unExecute is null)
+                throw new ArgumentNullException(nameof(_unExecute));
+            _unExecute(_state);
+        }
+
+        #endregion
+    }
 }
