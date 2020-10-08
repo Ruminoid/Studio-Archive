308403a702a86b3b3dba14480e60ac42eca9b79d
Il Harper <afanyiyu@hotmail.com>
2020-10-08 17:08:42 +0800

    feat: Add Transaction Class

---
+++ Ruminoid.Studio/Transaction/Transaction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.ViewModels;
+
+namespace Ruminoid.Studio.Transaction
+{
+    public class Transaction : MvxNotifyPropertyChanged, IAction
+    {
+        #region IAction
+
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnExecute()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
