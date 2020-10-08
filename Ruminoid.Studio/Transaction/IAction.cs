26a0e955463cdb4547f87b4d0e99f52de764bf06
Il Harper <afanyiyu@hotmail.com>
2020-10-08 16:51:53 +0800

    feat: Add IAction Interface

---
+++ Ruminoid.Studio/Transaction/IAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ruminoid.Studio.Transaction
+{
+    public interface IAction
+    {
+        void Execute();
+
+        void UnExecute();
+    }
+}
